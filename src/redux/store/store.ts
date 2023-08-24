9cd6b4d8580dab2742045a735b9c6ed8d714b4ca
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-08-24 02:33:12 +0200

    Basic store configuration and typed redux hooks have been created

---
+++ src/redux/store/store.ts
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const setUpStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setUpStore>;
+export type AppDispatch = AppStore["dispatch"];
